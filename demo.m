4a5899a8acdd072b65381d38f2f38b648efdf4a4
Jia Zhang <40818474+JiaZhang19@users.noreply.github.com>
2019-06-26 12:07:22 +0800

    Update demo.m

--- demo.m
+++ demo.m
@@ -1,7 +1,7 @@
 % This is an example file on how the MDFS [1] program could be used.
 
 % [1] J. Zhang, Z. Luo, C. Li, C. Zhou, S. Li: 
-% Manifold regularized discriminative feature selection for multi-label learning, to be appear in PR.
+% Manifold regularized discriminative feature selection for multi-label learning, Pattern Recognition, 2019, 95: 136-150.
 
 % Please feel free to contact me (zhangjia_gl@163.com), if you have any problem about this program.
 
